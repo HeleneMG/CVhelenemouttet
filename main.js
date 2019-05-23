02d82a50fdad12622668eda52b5c9decbb6c7598
HeleneMG <helenemouttetg@gmail.com>
2019-05-23 17:53:26 +0200

    heure

--- main.js
+++ main.js
@@ -6,25 +6,12 @@
     }
 }
 
-function dateFr() {
-    // les noms de jours / mois
-    var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
-    var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
-    // on recupere la date
-    var date = new Date();
-    // on construit le message
-    var message = jours[date.getDay()] + " ";   // nom du jour
-    message += date.getDate() + " ";   // numero du jour
-    message += mois[date.getMonth()] + " ";   // mois
-    message += date.getFullYear();
-    return message;
-}
 
-function heure() {
-    var date = new Date();
-    var heure = date.getHours();
-    var minutes = date.getMinutes();
-    if (minutes < 10)
-        minutes = "0" + minutes;
-    return heure + "h" + minutes;
-}
+    var myVar = setInterval(function () {myTimer()}, 1000);
+
+      function myTimer() {
+        var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = t;
+  }
+
